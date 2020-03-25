81d5ec80139c2ddee81fed162fabe74004b4e05a
Phillip Rousu <philliprousu@gmail.com>
2020-03-25 16:41:49 -0700

    init

---
+++ client/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import style from '../CSS/app.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+  }
+
+  render() {
+    return (
+      <div className={style.app}>
+       hi
+      </div>
+    );
+  }
+}
+
+export default App;
